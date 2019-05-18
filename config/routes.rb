1ce6b24ddb3b69afb76dde274b3dfed6d2afb3b3
TyyHunt <tyhunt04@gmail.com>
2019-05-18 09:09:03 -0700

    created root routing and created sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :games
   resources :arcades
   resources :players
+
+  root to: 'static#home'
+  get '/signin' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
