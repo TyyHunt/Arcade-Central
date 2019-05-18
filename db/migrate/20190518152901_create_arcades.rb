a9b0d848b71fd6e112a03d4a88cc183263c1701f
TyyHunt <tyhunt04@gmail.com>
2019-05-18 08:31:19 -0700

    filled in Arcade DB

--- db/migrate/20190518152901_create_arcades.rb
+++ db/migrate/20190518152901_create_arcades.rb
@@ -1,7 +1,13 @@
 class CreateArcades < ActiveRecord::Migration[5.2]
   def change
     create_table :arcades do |t|
-
+      t.string :name
+      t.string :location
+      t.integer :open_time
+      t.integer :close_time
+      t.integer :est_year
+      t.string :owner_name
+      
       t.timestamps
     end
   end
