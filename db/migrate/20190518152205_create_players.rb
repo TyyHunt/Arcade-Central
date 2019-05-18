2ebcc893f849c0ab7d889509f536f4631f213c00
TyyHunt <tyhunt04@gmail.com>
2019-05-18 08:24:04 -0700

    filled in DB info for Player

--- db/migrate/20190518152205_create_players.rb
+++ db/migrate/20190518152205_create_players.rb
@@ -1,7 +1,11 @@
 class CreatePlayers < ActiveRecord::Migration[5.2]
   def change
     create_table :players do |t|
-
+      t.string :username
+      t.integer :age
+      t.float :allowance
+      t.string :password_digest
+      
       t.timestamps
     end
   end
