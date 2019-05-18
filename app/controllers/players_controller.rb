3bc1d235f24aa478062d1ea1ddac3153d668cf8f
TyyHunt <tyhunt04@gmail.com>
2019-05-18 10:05:30 -0700

    adding actions and views to PlayersController

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,32 @@
 class PlayersController < ApplicationController
+    def show
+        @player = Player.find(params[:id])
+        if logged_in?
+            render :show
+        else
+            redirect_to root_path
+        end
+    end
+    
+    def new
+        @player = Player.new
+    end
+
+    def create
+        @player = Player.new(player_params)
+       
+        if @player.save
+            session[:user_id] = @player.id
+            redirect_to @player
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def player_params
+        params.require(:player).permit(:username, :age, :allowance, :password_digest)
+    end
+
 end
