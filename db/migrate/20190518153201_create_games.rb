17978b001fb4cf742752ee306437bf2a4e2cfe0e
TyyHunt <tyhunt04@gmail.com>
2019-05-18 08:33:52 -0700

    filled in Games DB table

--- db/migrate/20190518153201_create_games.rb
+++ db/migrate/20190518153201_create_games.rb
@@ -1,7 +1,13 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-
+      t.string :name
+      t.integer :num_players
+      t.float :cost
+      t.boolean :working
+      t.integer :arcade_id
+      t.integer :player_id
+      
       t.timestamps
     end
   end
