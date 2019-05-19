b6b3e98f3c5d16c0bb3a8b26b5cb4b3335846c4c
TyyHunt <tyhunt04@gmail.com>
2019-05-18 17:59:45 -0700

    creating show and index actions in GamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,14 @@
 class GamesController < ApplicationController
+
+    def show
+        @game = Game.find(params[:id])
+    end
+
+    def index
+        if params[:arcade_id]
+          @games = Game.find(params[:arcade_id]).posts
+        else
+          @games = Game.all
+        end
+    end
 end
