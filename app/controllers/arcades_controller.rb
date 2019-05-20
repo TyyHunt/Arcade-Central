76a9d9057600c0c6a080b3cb311331d7e7b59838
TyyHunt <tyhunt04@gmail.com>
2019-05-20 13:52:26 -0700

    adding validations for Arcade model

--- app/controllers/arcades_controller.rb
+++ app/controllers/arcades_controller.rb
@@ -21,6 +21,26 @@ class ArcadesController < ApplicationController
         @arcade = Arcade.find(params[:id])
     end
 
+    def edit
+        @arcade = Arcade.find_by(id: params[:id])
+        if @arcade.owner_name == current_user.username
+            render 'edit'
+        elsif @arcade == nil
+            redirect_to root_path
+        else
+            redirect_to @arcade
+        end
+    end
+
+    def update
+        @arcade = Arcade.find(params[:id])
+        if @Arcade.update(arcade_params(:name, :open_time, :close_time))
+            redirect_to @player
+        else
+            redirect to player_edit_path(@player)
+        end
+    end
+
     private
 
     def arcade_params(*args)
