c060da8ff79011d1220daaab59305826dd5745c7
TyyHunt <tyhunt04@gmail.com>
2019-05-20 11:42:59 -0700

    created new arcade action and view

--- app/controllers/arcades_controller.rb
+++ app/controllers/arcades_controller.rb
@@ -3,8 +3,13 @@ class ArcadesController < ApplicationController
     def index
         @arcades = Arcade.all
     end
+
     def show
         @arcade = Arcade.find(params[:id])
     end
-    
+
+    def new
+        @arcade = Arcade.new
+    end
+
 end
