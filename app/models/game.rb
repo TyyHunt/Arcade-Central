c6cccb7fee56314fd29d716ed976c5c33d7b4f42
TyyHunt <tyhunt04@gmail.com>
2019-05-20 18:47:03 -0700

    added validations to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,9 @@ class Game < ApplicationRecord
     belongs_to :arcade
     belongs_to :player
 
+    validates :name, :num_players, :cost, :working, :arcade_id, presence: true
+    validates :name, uniqueness: true
+    validates_inclusion_of :num_players, :in => 1..6
+    
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,10 @@
 class Player < ApplicationRecord
     has_secure_password
+    
+    has_many :games
+    has_many :arcades, through: :games
+
     validates :username, presence: true
     validates :username, uniqueness: true
     validates :password, length: { in: 6..20 }
-
-    has_many :games
-    has_many :arcades, through: :games
 end
