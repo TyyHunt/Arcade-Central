736cc22d2a767f00e758e49bc1a5483b0fad60ea
TyyHunt <tyhunt04@gmail.com>
2019-05-24 16:09:46 -0700

    editing actions for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,31 @@
 class SessionsController < ApplicationController
     def new
-        @player = Player.new
+        if current_user
+            redirect_to '/'
+        else
+            @player = Player.new
+        end
     end
-
+    
+    def create 
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+          @user.save
+          session[:user] = @user
+          redirect_to root_path
+        else
+          redirect_to sign_up_path
+        end
+      end
+      
     def create
         @player = Player.find_by(username: params[:username])
-        if @player
+        if @player && @player.authenticate(params[:player][:password])
+            @user.save
             session[:player_id] = @player.id
             redirect_to player_path(@player)
         else
-            render :new
+            redirect_to 'signup'
         end
     end
 
