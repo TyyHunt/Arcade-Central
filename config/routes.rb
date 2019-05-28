4ec19dbd9c5dbbc902aafe3c1720d82cead4fbf1
TyyHunt <tyhunt04@gmail.com>
2019-05-28 11:11:13 -0700

    troubleshooting github omniauth routing

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ resources :arcades do
   root 'static#home'
   get '/signup' => 'players#new'
   get '/signin' => 'sessions#new'
-  get '/auth/github/callback', to: 'sessions#github'
+  match  '/auth/github/callback', to: 'sessions#github', via: [:get, :post] 
   post '/signin' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   delete '/logout', to: 'sessions#destroy'
