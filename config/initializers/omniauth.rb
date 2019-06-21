0d24568b2291de4c828680e5387d3ec5506d295e
TyyHunt <tyhunt04@gmail.com>
2019-06-21 10:54:59 -0700

    hid github key and github secret outside of omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, '910e7773399ca1fd6cfd', 'ff5289463ef13adcb24b210a6e9cd2a7abe5869b'
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
 end
-# Rails.application.secrets.github_client_id, Rails.application.secrets.github_client_secret
