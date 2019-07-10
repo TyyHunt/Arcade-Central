f645ae3c0f7a096e01c5dda6f646629c50b72327
TyyHunt <tyhunt04@gmail.com>
2019-07-10 14:34:36 -0700

    creating listener event for button click

--- app/assets/javascripts/arcades.js
+++ app/assets/javascripts/arcades.js
@@ -1,3 +1,16 @@
 $(function() {
     console.log( "arcades ready!" );
+    arcadeIndexClick()
 });
+
+function arcadeIndexClick() {
+    $('button.arcade-names').on('click', function (event) {
+        event.preventDefault()
+        console.log("works")
+        getArcade()
+    })
+}
+
+function getArcade() {
+    document.getElementsByID('arcade-show').innerHTML = "Hello"
+}
