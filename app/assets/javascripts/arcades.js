a192dd2d77e43844d96c30bc8e25ba44b4eac7f8
TyyHunt <tyhunt04@gmail.com>
2019-07-10 15:25:36 -0700

    started the ajax get request

--- app/assets/javascripts/arcades.js
+++ app/assets/javascripts/arcades.js
@@ -1,16 +1,37 @@
 $(function() {
-    console.log( "arcades ready!" );
+    console.log( "arcades ready!" )
     arcadeIndexClick()
 });
 
 function arcadeIndexClick() {
-    $('button.arcade-names').on('click', function (event) {
+    console.log( "entered arcade index click" )
+    $('button#arcade-names').on('click', function (event) {
         event.preventDefault()
-        console.log("works")
         getArcade()
     })
 }
 
 function getArcade() {
-    document.getElementsByID('arcade-show').innerHTML = "Hello"
+    console.log( "entered get arcade" )
+    $.ajax({
+        url: 'http://localhost:3000/arcades',
+        method: 'get',
+        dataType: 'json'
+    }).done( function (data) {
+        console.log("the data is", data)
+
+    })
+    document.getElementById('arcade-show').innerHTML = "Hello"
+}
+
+class Arcade {
+    constructor(obj) {
+        this.id = obj.id
+        this.name = obj.name
+        this.location = obj.location
+        this.openTime = obj.open_time
+        this.closeTime = obj.close_time
+        this.estYear = obj.est_year
+        this.games = obj.games
+    }
 }
