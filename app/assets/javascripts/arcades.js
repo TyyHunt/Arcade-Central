2fb62f4dce5df5b44fa1218ef38470a767ed4735
TyyHunt <tyhunt04@gmail.com>
2019-07-15 10:38:01 -0700

    finished js integration to arcades index page

--- app/assets/javascripts/arcades.js
+++ app/assets/javascripts/arcades.js
@@ -6,44 +6,38 @@ $(function() {
 function clickSpecific() {
     document.body.addEventListener("click", event => {
         if (event.target.nodeName == "BUTTON") {
-            let arcadeName = $(event.target)[0].innerText
-            let arcadeData = 
+            let arcadeName = $(event.target)[0].innerText.toString();
             $.get('/arcades.json' , function (data) { 
-                console.log("the data is", arcadeName)
+                let result = data.filter(obj => {
+                    if (obj.name === arcadeName) {
+                        return obj;
+                      }
+                  })
+                let jsArcade = new Arcade(result[0])
                 arcadeHtml = HandlebarsTemplates['show_arcade']({ 
-                    name: arcadeName  
+                    name: jsArcade.name,
+                    location: jsArcade.location,
+                    ownerName: jsArcade.ownerName,
+                    openTime: jsArcade.openTime,
+                    closeTime: jsArcade.closeTime,
+                    estYear: jsArcade.estYear,
+                    id: jsArcade.id  
                 });
                 $('#arcade-show').html(arcadeHtml); 
             });
-        }
-        
+        } 
       });
 }
-
-Arcade.prototype.singleArray = function (array, input) {
-    array.map
-}
-
-function getArcade() {
-    console.log( "entered get arcade" )
-    $.get('/arcades.json' , function (data) { 
-        console.log("the data is", data)
-        arcadeHtml = HandlebarsTemplates['show_arcade']({ 
-            name: data  
-        });
-        $('#arcade-show').html(arcadeHtml); 
-    });
-}  
 
 class Arcade {
     constructor(obj) {
         this.id = obj.id
         this.name = obj.name
         this.location = obj.location
-        this.owner_name = obj.owner_name
-        this.open_time = obj.open_time
-        this.close_time = obj.close_time
-        this.est_year = obj.est_year
+        this.ownerName = obj.owner_name
+        this.openTime = obj.open_time
+        this.closeTime = obj.close_time
+        this.estYear = obj.est_year
         this.games = obj.games
     }
 }
