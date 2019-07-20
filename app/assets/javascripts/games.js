51766431b298336381e0190cf14595acf43d439f
TyyHunt <tyhunt04@gmail.com>
2019-07-20 11:59:22 -0700

    ajax request for show page now complete

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -6,18 +6,21 @@ $(function() {
 function clickGameButton() {
     $('a').click(function(event) {
         event.preventDefault()
-        showGame()
+        $.ajax({
+            url: $(this).attr('href'),
+            method: 'get',
+            dataType: 'json',
+        }).done(function(data) {
+            console.log('here is your data...', data)
+            renderGame(data)
+        })  
     });
 }
 
-function showGame() {
-    $.ajax({
-        url: $(this).attr('href'),
-        method: 'get',
-        dataType: 'json',
-    }).done(function(data) {
-        console.log('here is your data...', data)
-    })
+function renderGame(obj) {
+    let jsGame = new Game(obj)
+    let postGame = jsGame.showGamePage()
+    document.getElementById("game-holder").innerHTML = postGame
 }
 
 class Game {
@@ -29,4 +32,21 @@ class Game {
         this.working = obj.working
     }
 }
+
+Game.prototype.showGamePage = function () {
+    return (`
+    <div class="card text-white bg-secondary mb-3 float-left inline" style="max-width: 18rem; height: 400px; width: 700px; margin-left: 400px;">
+        <div class="card-header">
+            <h4>${this.name}</h4>
+        </div>
+        <div class="card-body">
+            <p class="card-text">Max Players: ${this.numPlayers}</p>
+            <p class="card-text">Costs: ${this.cost} cents</p>
+            <p class="card-text">In Order: ${this.working}</p>
+        </div>
+    </div>
+    `)
+}
+
+
 
