f1660f9b8ec106a9a13b5409d810ce3b69d34278
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2019-10-24 12:01:22 -0600

    switched from sqlite to postgres

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
+  provider :github, '910e7773399ca1fd6cfd', 'ff5289463ef13adcb24b210a6e9cd2a7abe5869b'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_05_18_153201) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "arcades", force: :cascade do |t|
     t.string "name"
     t.string "location"
